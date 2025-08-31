22cdeb49115be9f355746feaadff8cfb53141a30
hongui <honge7694@naver.com>
2025-09-01 00:17:42 +0900

    feat: LLM response call 방식, stream 방식

--- src/main/java/kr/hui/springai/rest/SimpleChatController.java
+++ src/main/java/kr/hui/springai/rest/SimpleChatController.java
@@ -1,8 +1,11 @@
 package kr.hui.springai.rest;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 
 @RestController
 public class SimpleChatController {
@@ -14,7 +17,17 @@ public class SimpleChatController {
     }
 
     @GetMapping("ai")
-    String generation(String userPrompt) {
+    public String generation(String userPrompt) {
         return this.chatClient.prompt().user(userPrompt).call().content();
+    }
+
+    @GetMapping(value = "call", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ChatResponse call(String userPrompt) {
+        return this.chatClient.prompt().user(userPrompt).call().chatResponse();
+    }
+
+    @GetMapping(value = "stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<String> stream(String userPrompt) {
+        return this.chatClient.prompt().user(userPrompt).stream().content();
     }
 }
