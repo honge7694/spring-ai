73a9e8619e5b2ae7690d2cb7113b6a7c2dcc5429
홍의 <u.hong@weaving.co.kr>
2025-10-03 16:28:34 +0900

    feat: Chat Memory Advisors 설정

---
+++ src/main/java/kr/hui/springai/config/SimpleChatConfig.java
@@ -0,0 +1,33 @@
+package kr.hui.springai.config;
+
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.memory.MessageWindowChatMemory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SimpleChatConfig {
+
+    /* 디버깅 및 모니터링에 유용하며, 기본 로깅 포맷과 커스터마이징 기능을 지원 */
+    @Bean
+    public SimpleLoggerAdvisor simpleLoggerAdvisor() {
+        return SimpleLoggerAdvisor.builder().order(0).build(); // order()를 안줘도 기본은 0
+    }
+
+    /*
+    * MessageWindowChatMemory를 사용해 최근 메시지를 유지하는 채팅 메모리
+    * 초과 시 오래된 메시지를 순나적으로 제거하며 SystemMessage는 보관하지 않음
+    *  */
+    @Bean
+    public ChatMemory chatMemory() {
+        return MessageWindowChatMemory.builder().maxMessages(10).build();
+    }
+
+    /* ChatClient 호출 전후에 chatMemory를 이용해 대화 내역을 프롬프트에 자동으로 주입하거나 응답 저장을 수행 */
+    @Bean
+    public MessageChatMemoryAdvisor messageChatMemoryAdvisor(ChatMemory chatMemory) {
+        return MessageChatMemoryAdvisor.builder(chatMemory).build();
+    }
+}
