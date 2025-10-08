6f958ccfc868a8040eb59c1046c46200e849fe5f
홍의 <u.hong@weaving.co.kr>
2025-10-08 14:50:50 +0900

    feat: Spring AI RAG - RAG Advisors 개발
    - RAG Frame Work인 Advisors를 개발

--- src/main/java/kr/hui/springai/rag/RagConfig.java
+++ src/main/java/kr/hui/springai/rag/RagConfig.java
@@ -2,14 +2,21 @@ package kr.hui.springai.rag;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.document.DocumentReader;
 import org.springframework.ai.document.DocumentTransformer;
 import org.springframework.ai.document.DocumentWriter;
 import org.springframework.ai.embedding.EmbeddingModel;
 import org.springframework.ai.model.transformer.KeywordMetadataEnricher;
+import org.springframework.ai.rag.advisor.RetrievalAugmentationAdvisor;
+import org.springframework.ai.rag.generation.augmentation.ContextualQueryAugmenter;
+import org.springframework.ai.rag.preretrieval.query.expansion.MultiQueryExpander;
+import org.springframework.ai.rag.preretrieval.query.transformation.TranslationQueryTransformer;
+import org.springframework.ai.rag.retrieval.search.VectorStoreDocumentRetriever;
 import org.springframework.ai.reader.tika.TikaDocumentReader;
 import org.springframework.ai.vectorstore.SimpleVectorStore;
+import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
@@ -54,7 +61,7 @@ public class RagConfig {
     }
 
     @Bean
-    public DocumentWriter vectorStore(EmbeddingModel embeddingModel) {
+    public VectorStore vectorStore(EmbeddingModel embeddingModel) {
         return SimpleVectorStore.builder(embeddingModel).build();
     }
 
@@ -71,5 +78,20 @@ public class RagConfig {
                     .forEach(documents -> Arrays.stream(documentWriters) // vectorStore 저장 (Load)
                             .forEach(documentWriter -> documentWriter.write(documents)));
         };
+    }
+
+    @Bean
+    public RetrievalAugmentationAdvisor retrievalAugmentationAdvisor(VectorStore vectorStore, ChatClient.Builder chatClientBuilder) {
+
+        RetrievalAugmentationAdvisor.Builder documentRetrieverBuilder = RetrievalAugmentationAdvisor.builder()
+                .queryExpander(MultiQueryExpander.builder().chatClientBuilder(chatClientBuilder).build()) // 쿼리 확장기 다수의 쿼리를 하나의 결과로 묶어줌 (LLM 도움 필요)
+                .queryTransformers(TranslationQueryTransformer.builder().chatClientBuilder(chatClientBuilder).targetLanguage("ko").build()) // 한국어로 바꿔주는 역할 (LLM 도움 필요)
+                .queryAugmenter(ContextualQueryAugmenter.builder().allowEmptyContext(true).build()) //
+                .documentRetriever(VectorStoreDocumentRetriever.builder()
+                        .vectorStore(vectorStore)
+                        .similarityThreshold(0.3) // 값을 조정하며 튜닝해야 함
+                        .topK(3)
+                        .build());
+        return documentRetrieverBuilder.build();
     }
 }
